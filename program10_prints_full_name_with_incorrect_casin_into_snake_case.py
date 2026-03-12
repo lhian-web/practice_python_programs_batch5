d4e85fb1bf8a847af4a0f8b4651191ede3732628
unknown <jhullianchica@gmail.com>
2026-03-13 00:44:32 +0800

    Added input, split full name to words, converted words to lowercase, added underscore in between words, printed full name in snake case

--- program10_prints_full_name_with_incorrect_casin_into_snake_case.py
+++ program10_prints_full_name_with_incorrect_casin_into_snake_case.py
@@ -1,5 +1,17 @@
 # Input full name
+full_name = input("Enter your full name (incorrect casing): ")
+
 # Split into words
+words = full_name.split()
+snake_case = ""
+
 # Lowercase every word
-# Add underscore
+for i in range(len(words)):
+    word = words[i].lower()
+    snake_case += word
+    # Add underscore
+    if i < len(words) - 1:
+        snake_case += "_"
+
 # print in snake casing
+print(snake_case)
