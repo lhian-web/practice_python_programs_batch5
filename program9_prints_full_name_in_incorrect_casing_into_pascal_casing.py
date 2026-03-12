7b63dbe5d850a543629a8209dd655154247d4a61
unknown <jhullianchica@gmail.com>
2026-03-13 00:35:59 +0800

    Added input, split full name into words, capitalized first letters, combined words, printed full name in pascal casing

--- program9_prints_full_name_in_incorrect_casing_into_pascal_casing.py
+++ program9_prints_full_name_in_incorrect_casing_into_pascal_casing.py
@@ -1,4 +1,13 @@
 # Input full name
+full_name = input("Enter your full name (incorrect casing): ")
+
 # Split into words
+words = full_name.split()
+pascal = ""
+
 # Capitalize every first letter of each word and combine
+for word in words:
+    pascal += word.capitalize()
+
 # Print
+print(pascal)
